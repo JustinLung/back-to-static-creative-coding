bb3f17cc729e20f4bb869fc7503f7fd78d55ae68
JustinLung <justinlung77@gmail.com>
2022-12-06 17:26:58 +0100

    initial commit

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,9 @@
+<header>
+	<h1>Creative Coding</h1>
+	<nav>
+		<a href="/">Home</a>
+		<a href="/">About</a>
+		<a href="/">Projects</a>
+		<a href="/">Contact</a>
+	</nav>
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Header from '$lib/components/Header.svelte';
+	import '../styles/global.css';
+</script>
+
+<Header />
+<main>
+	<slot />
+</main>
