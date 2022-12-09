c8cc855cf47f4e3d8297c962903dada31ae65d3a
JustinLung <justinlung77@gmail.com>
2022-12-09 12:24:16 +0100

    added projects and home component

---
+++ src/lib/components/Home.svelte
@@ -0,0 +1,35 @@
+<section id="home">
+	<h2>Hi, Welcome to this site!</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolore quis ullam aliquid ut
+		dolorem voluptates quisquam animi quibusdam labore totam non id, similique tempore, odio
+		consectetur nihil temporibus obcaecati!
+	</p>
+</section>
+
+<style>
+	section {
+		height: 30rem;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		background-color: var(--color-wine);
+		padding: 1rem 3rem;
+	}
+	h2 {
+		font-size: 3rem;
+		margin: 0;
+	}
+	p {
+		margin: 0;
+		max-width: 30rem;
+		font-size: 1.2rem;
+		line-height: 1.5rem;
+	}
+
+	@media (max-width: 55rem) {
+		section {
+			padding: 1rem;
+		}
+	}
+</style>
---
+++ src/lib/components/Projects.svelte
@@ -0,0 +1,31 @@
+<section id="projects">
+	<h2>Projects</h2>
+    <div>
+        <img src="https://source.unsplash.com/random" alt="Projects">
+        <img src="https://source.unsplash.com/random" alt="Projects">
+        <img src="https://source.unsplash.com/random" alt="Projects">
+        <img src="https://source.unsplash.com/random" alt="Projects">
+        <img src="https://source.unsplash.com/random" alt="Projects">
+    </div>
+</section>
+
+<style>
+    section {
+        max-width: 55rem;
+        margin: 0 auto;
+    }
+
+    div {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+        gap: 1rem;
+        width:100%;
+    }
+
+    img {
+        max-width: 25rem;
+    }
+</style>
