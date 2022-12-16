2d3c599d9734b095c8979daa78f474b47244cc6e
JustinLung <justinlung77@gmail.com>
2022-12-16 10:15:39 +0100

    WIP Cursor

---
+++ src/lib/components/Links.svelte
@@ -0,0 +1,33 @@
+<section>
+	<nav>
+		<a href="/"><span>Home</span></a>
+		<a href="/"><span>Our Story</span></a>
+		<a href="/"><span>Studio</span></a>
+		<a href="/"><span>Contact</span></a>
+		<div class="cursor" />
+	</nav>
+</section>
+
+<style>
+    section {
+        height: 70vh;
+        width: 100%;
+    }
+
+    nav {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        gap: 2rem;
+    }
+
+    a {
+        font-size: 2.5rem;
+        text-decoration: none;
+        text-transform: uppercase;
+        font-weight: 900;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Home from "$lib/components/Home.svelte";
-	import Projects from "$lib/components/Projects.svelte";
+	import Links from "$lib/components/Links.svelte";
+	// import Projects from "$lib/components/Projects.svelte";
 </script>
 
 <Home/>
-<Projects/>
+<!-- <Projects/> -->
+<Links/>
